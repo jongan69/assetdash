b80fc0a347f625f71599c6f16618ddb36ea4c271
Jonathan Gan <jonathang132298@gmail.com>
2024-02-15 09:56:11 -0500

    whoops

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
 
   // Create a Supabase client configured to use cookies
-  const supabase = createMiddlewareClient<>({ req, res })
+  const supabase = createMiddlewareClient({ req, res })
 
   // Refresh session if expired - required for Server Components
   await supabase.auth.getSession()
