5b93faa40cfcfb87f144e03f3d03571e46d65ab0
Jonathan Gan <jonathang132298@gmail.com>
2024-02-15 00:37:27 -0500

    ok

--- custom-alpaca.d.ts
+++
@@ -1,6 +0,0 @@
-// custom-alpaca.d.ts
-import { AlpacaClient as OriginalAlpacaClient } from '@master-chief/alpaca/@types/client';
-
-declare module '@master-chief/alpaca' {
-  export type AlpacaClient = OriginalAlpacaClient;
-}
