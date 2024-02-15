068c635ed86254781e3bfaa343d582985dfec379
Jonathan Gan <jonathang132298@gmail.com>
2024-02-15 09:55:54 -0500

    yes

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,12 @@ import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
 
 import type { NextRequest } from 'next/server'
-import type { Database } from '@/lib/database.types'
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
 
   // Create a Supabase client configured to use cookies
-  const supabase = createMiddlewareClient<Database>({ req, res })
+  const supabase = createMiddlewareClient<>({ req, res })
 
   // Refresh session if expired - required for Server Components
   await supabase.auth.getSession()
