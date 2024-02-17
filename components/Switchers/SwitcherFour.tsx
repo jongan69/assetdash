b2e5c1122d8355421365080b1536f56bc889ec35
Jonathan Gan <jonathang132298@gmail.com>
2024-02-16 21:13:55 -0500

    Working

--- components/Switchers/SwitcherFour.tsx
+++ components/Switchers/SwitcherFour.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 
-const SwitcherFour = ({ enabled, setEnabled}) => {
+const SwitcherFour = ({ enabled, setEnabled}: any) => {
   // const [enabled, setEnabled] = useState<boolean>(false);
 
   return (
